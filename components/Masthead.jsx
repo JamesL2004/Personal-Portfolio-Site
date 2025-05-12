5531056a468db5a7fbfa06486f48603535bda3ce
James Lynch <jdlynch@lakeheadu.ca>
2025-05-12 18:25:46 -0400

    Made small change to masthead

--- components/Masthead.jsx
+++ components/Masthead.jsx
@@ -12,14 +12,14 @@ const robotoBoldFont = Roboto_Condensed({
 
 export default function Masthead() {
     return (
-        <div className={`flex flex-col items-center justify-center mx-auto p-2 md:flex-row space-y-4 space-x-16`}>
-            <div className={`max-w-md text-center md:text-left`}>
+        <section className={`flex flex-col items-center justify-center mx-auto p-2 md:flex-row space-y-4 space-x-16`}>
+            <div className={`max-w-md text-center`}>
                 <h2 className={`text-2xl font-bold font-mb-2 text-blue-400 ${robotoBoldFont.className}`}>Hi, I'm James</h2>
                 <p className={`text-md ${robotoFont.className}`}>
                     This is my Personal Portfolio Site where you can see the projects I'm proudest of
                     and see a bit of my life story.
                 </p>
             </div>
-        </div>
+        </section>
     );
 }
