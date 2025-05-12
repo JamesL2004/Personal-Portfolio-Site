282d6b01b7bd10caeac94027fcae190ed06eaf13
James Lynch <jdlynch@lakeheadu.ca>
2025-05-12 17:19:57 -0400

    Updated Job experience and education sections to make them responsive

--- components/EducationHistory.jsx
+++ components/EducationHistory.jsx
@@ -3,8 +3,8 @@ export default function EducationHistory() {
         <>
             <section id="educ">
                 <h2 className={`text-center font-bold text-2xl`}>Education</h2>
-                <div className={`flex items-center justify-center space-x-30`}>
-                    <img src="/images/lakehead-georgian.png"/>
+                <div className={`flex flex-col items-center justify-center space-y-10 md:flex-row md:space-x-10 md:space-y-0`}>
+                    <img src="/images/lakehead-georgian.png" className={`max-w-md h-auto`}/>
                     <div className={`items-center justify-center`}>
                         <h3 className={`font-bold text-2xl pb-4`}>Lakehead-Georgian Honours Bachelor's of Science in Computer Science Combined Credintial Program</h3>
                         <h4 className={`pb-1 text-lg`}>Completed Courses</h4>
