b581c440ca7a60814e8bd10f0f73410e01552cc5
James Lynch <jdlynch@lakeheadu.ca>
2025-05-14 17:13:08 -0400

    Updated about me section with actual content

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -15,14 +15,14 @@ export default function AboutMe(){
         <>
             <section id="about-me" className={`py-10`}>
             <div className="bg-blue-400 max-w-2xl mx-auto flex flex-col md:flex-row md:max-w-7xl p-10 rounded-xl transition-transform duration-300 hover:-translate-y-1 hover:shadow-lg">
-                <img src="/images/masthead_image.png" className="w-full rounded-full border md:w-1/3" /> 
+                <img src="/images/masthead_image.png" className="w-full rounded-full md:w-1/3" /> 
                 <div className="w-full space-y-2 p-4 flex flex-col items-center justify-center">
                     <h2 className={`pt-2 text-2xl ${robotoBoldFont.className}`}>About Me</h2>
                     <p className={`text-center max-w-7xl ${robotoFont.className}`}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac elit mi. Sed hendrerit, sapien pulvinar pellentesque tempor, felis sapien efficitur mi, vel elementum mauris leo sit amet felis. Praesent tristique mi felis, eget vehicula urna congue tristique. 
-                        Etiam at ex aliquet erat lacinia rhoncus elementum ut tortor. 
-                        Quisque vitae nisi non mauris molestie cursus a tristique augue. 
-                        Vivamus semper mauris in libero ullamcorper consequat. Donec non nulla justo.
+                        Hi I'm James. I am currently a student at Lakehead University studying Computer Science with one day hoping to follow my passion by becoming a full-time programmer. 
+                        I grew up in Lively, Ontario and went to school at Lockeryby Composite School. In 2022 I graduated from Lockerby with honours and with a SHSM seal in Information Systems and Technology.
+                        Currently, I am looking for part-time job oppurtunities in Orillia, Ontario to gain more work experience. I am a passionate, hard-working, and responsible person that any employer can rely on.
+                        Thank you for taking the time to review my application and taking a look at my website.
                     </p>
                 </div>
             </div>   
