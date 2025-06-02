317db5c6f75bb7b078e575795367d071f0a6ac92
James Lynch <jdlynch@lakeheadu.ca>
2025-06-02 14:25:38 -0400

    added return statement for WumpusInfo component

--- components/wumpusWorld/WumpusInfo.jsx
+++ components/wumpusWorld/WumpusInfo.jsx
@@ -8,21 +8,24 @@ let slides = [
     ];
 
 export default function WumpusInfo() {
-    <section id="wumpus-info">
-        <div className={`flex items-center justify-center space-x-20 max-w-full mx-auto bg-blue-400 rounded-lg p-20 transition-transform duration-300 hover:-translate-y-1 hover:shadow-lg`}>
-            <div>
-                <ImageCarousel slides={slides}/>
+    return (
+        <section id="wumpus-info">
+            <div className={`flex items-center justify-center space-x-20 max-w-full mx-auto bg-blue-400 rounded-lg p-20 transition-transform duration-300 hover:-translate-y-1 hover:shadow-lg`}>
+                <div>
+                    <ImageCarousel slides={slides}/>
+                </div>
+                <div>
+                    <h2 className="font-bold text-2xl">LLM Agent Wumpus World</h2>
+                    <p className={`text-left`}>
+                        After focusing on web and app development in my first two years, when I entered my third year I decided I would focus more on Machine Learning and data science to diversify
+                        my knowledge. During those two semesters I took three different classes that related to ML. Big Data, Intro to Data Science, and Intro to Artifical Intelligence. 
+                        This was my final project for the Intro to Artifical Intelligence. This project is a simulation of the classic wumpus world problem using the phi-4 SLM as the hero's "brain".
+                        It uses the SLM to make every decision the hero makes. This includes which space to move next, if it wants to try and shoot the wumpus, while also having a internal monolouge for each step.
+                        The hero keeps track of past moves and events to get better results from the SLM. It was built using Python's mesa library for agents, and solara for visualization.
+                    </p>
+                </div>
             </div>
-            <div>
-                <h2 className="font-bold text-2xl">LLM Agent Wumpus World</h2>
-                <p className={`text-left`}>
-                    During my third year at Lakehead University I took there Game Programming course. For my final project I decided to create Gobble Gauntlet.
-                    It is a top-down dungeon crawler that has the player go through different levels to transed the dungeon. While exploring these levels the player 
-                    needs to avoid and destroy enemies that will get in there way. Each level is designed so the player has to make a decision on which route to take. 
-                    Depending on the route it may make his attempt harder or easier, some routes may have tons of enemies, while some may have only a few enemies while also having powerups
-                    that can help the player of there journey.
-                </p>
-            </div>
-        </div>
-    </section>
+        </section>
+    )
+    
 };
